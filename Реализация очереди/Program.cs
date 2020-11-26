529f436d79721ad5143ec7cedd5f054cd46c07dd
devts145 <61932703+devts145@users.noreply.github.com>
2020-11-26 13:47:09 +0300

    True
    All is made

---
+++ Реализация очереди/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Реализация_очереди
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Queue<int> queue = new _Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            //Console.WriteLine("DSDJL");
+            //queue.printQueue();
+            //Console.WriteLine("asjcbjasbjckbajsk");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+
+            queue.Enqueue(124);
+            queue.Enqueue(777);
+
+            Console.WriteLine(queue.Dequeue());
+            queue.Clear();
+            //Console.WriteLine("AJKCKJBASJKBCJKAS");
+            Console.WriteLine(queue.Peek());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Реализация очереди/_Queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Реализация_очереди
+{
+    class _Queue<T>
+    {
+        private List<T> items = new List<T>();
+        public _Queue() { }
+        public _Queue(T val)
+        {
+            items.Add(val);
+        }
+        public T Back
+        {
+            get
+            {
+                return items.First();
+            }
+        }
+        public T Front ()
+        {
+            return items.Last();
+        }
+        public int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+        public T Peek()
+        {
+            try
+            {
+                return this.Front();
+            }
+            catch
+            {
+                //throw new Exception("Обращение к пустой очереди!");
+                return default(T);
+            }
+        }
+        public void Enqueue(T val)
+        {
+            items.Insert(0, val);
+        }
+        public T Dequeue()
+        {
+            try
+            {
+                var item = Front();
+                items.Remove(item);
+                return item;
+            }
+            catch
+            {
+                //throw new Exception("Обращение к пустой очереди!");
+                return default(T);
+            }
+        }
+        public bool isEmpty()
+        {
+            if (items.Count == 0)
+            {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public void Clear()
+        {
+            items.Clear();
+        }
+        public void printQueue()
+        {
+            foreach (T item in this.items) Console.WriteLine(item);
+        }
+    }
+}
